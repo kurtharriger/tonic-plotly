7e43e29697dd2fd424b49767e2aba92339678632
Kurt Harriger <kurtharriger@gmail.com>
2016-10-08 14:00:18 -0600

    fix end tag

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ module.exports = function plotly(data) {
     return '<div id="plotly"/>' +
       '<script type="text/javascript" src="https://cdn.plot.ly/plotly-latest.min.js"></script>' +
       '<script>Plotly.newPlot("plotly", ' + JSON.stringify(data)+ ');</script>' +
-      '</script>';
+      '</div>';
 }
